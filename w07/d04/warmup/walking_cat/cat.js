5274e38a30ad047e2ff62c20a930576a9d0f305c
Zakk <zachary.fleischmann@gmail.com>
2015-04-30 10:32:55 -0400

    finish morning exercise

--- w07/d04/warmup/walking_cat/cat.js
+++ w07/d04/warmup/walking_cat/cat.js
@@ -1,5 +1,19 @@
 walkingCat();
+body = document.getElementById('body');
 
 function walkingCat() {
+  var cat = document.getElementById('cat');
+  var catPositionLeft =  cat.style.left;
 
+  if (catPositionLeft.length === 0) {
+    newCatPosition = "10px";
+  } else if (catPositionLeft === "1100px"){
+    newCatPosition = "";
+  } else {
+    var currentVal = parseInt(catPositionLeft);
+    newCatPosition = currentVal + 10 + "px";
+  };
+  cat.style.left = newCatPosition;
 }
+
+var intervalID = window.setInterval(walkingCat, 200);
