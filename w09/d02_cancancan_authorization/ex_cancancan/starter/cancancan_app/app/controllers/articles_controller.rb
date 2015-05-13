33e42c9136dd9353a303dbc059326e8b280f73b2
Zakk <zachary.fleischmann@gmail.com>
2015-05-13 11:32:34 -0400

    optional cancancan lecture

--- w09/d02_cancancan_authorization/ex_cancancan/starter/cancancan_app/app/controllers/articles_controller.rb
+++ w09/d02_cancancan_authorization/ex_cancancan/starter/cancancan_app/app/controllers/articles_controller.rb
@@ -1,19 +1,16 @@
 class ArticlesController < ApplicationController
+  load_and_authorize_resource
 
   def index
-    @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
-    @article = Article.new
   end
 
   def create
-    @article = Article.new(article_params)
     @article.user = current_user
     if @article.save
       redirect_to @article
@@ -23,11 +20,9 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to @article
     else
@@ -36,7 +31,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
