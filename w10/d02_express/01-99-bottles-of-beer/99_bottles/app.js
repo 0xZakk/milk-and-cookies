bc57a9d536ad21fbf9fbac367e70d9546dd025ca
Zakk <zachary.fleischmann@gmail.com>
2015-05-19 11:37:28 -0400

    finish 99 bottles exercise

---
+++ w10/d02_express/01-99-bottles-of-beer/99_bottles/app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res) {
+  res.send("99 bottles of beer on the wall <a href='/98'>Take one down, pass it around</a>");
+});
+
+app.get("/0", function(req, res){
+  res.send("Oh no! 0 bottles of beer on the wall! :( <a href='/99'>Start over?</a>");
+});
+
+app.get("/:number_of_bottles", function(req, res){
+  res.send(req.params.number_of_bottles + " bottles of beer on the wall <a href='" + (req.params.number_of_bottles - 1 )+ "'>Take one down, pass it around</a>");
+});
+
+app.listen(3000, function(){
+  console.log("Hey! Your app is listening on port 3000");
+});
