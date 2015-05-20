5dc7d4fdad88e87b799987e8c62023273bce5246
Zakk <zachary.fleischmann@gmail.com>
2015-05-20 16:04:46 -0400

    finish day 3 morning exercise

--- w10/d03_warmup/js/src/pokemon.js
+++ w10/d03_warmup/js/src/pokemon.js
@@ -3,20 +3,39 @@ var pokemon = {
   pokemonList : allPokemon,
 
   printAllPokemonNamesToConsole : function(){
+    _.each(allPokemon, function(poke) {
+      console.log(poke.name);
+    });
   },
 
   findPokemonByName : function(name){
+    return _.findWhere(allPokemon, {name: name});
   },
 
   findStrongestPokemon : function(){
+    return _.max(allPokemon, function(poke){
+      return parseInt(poke.stats.attack);
+    });
   },
 
   findPokemonByType : function(type){
+    return _.filter(allPokemon, function(poke){
+      return _.contains(poke.type, type);
+    });
   },
 
   findAllTypes : function(){
+    var arrayOfPokeTypes = [];
+    _.each(allPokemon, function(poke) {
+      _.each(poke.type, function(type){
+        if (_.find(arrayOfPokeTypes, type)) {
+          arrayOfPokeTypes.push(type);
+        }
+      });
+    });
+    return arrayOfPokeTypes;
   },
 
   totalStats : function(name){
   }
-}
+};
